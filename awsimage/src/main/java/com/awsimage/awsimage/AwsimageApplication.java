2aff6309d166fbd70dec630f24fecb78bd1f645c
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-06-05 16:57:29 +0530

    Spring initializer and aws depency

---
+++ awsimage/src/main/java/com/awsimage/awsimage/AwsimageApplication.java
@@ -0,0 +1,13 @@
+package com.awsimage.awsimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsimageApplication.class, args);
+	}
+
+}
---
+++ awsimage/src/test/java/com/awsimage/awsimage/AwsimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsimage.awsimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
